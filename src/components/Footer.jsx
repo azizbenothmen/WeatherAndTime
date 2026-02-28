afb7994da93d7df54bd35dd8b805989ff160ec4e
azizbenothmen <aziz.benothmen@enicar.ucar.tn>
2026-02-28 02:25:35 +0100

    feat:add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="w-420 h-20 bg-blue-600 border-t border-white/10  -translate-x-30 font-serif ">
+      <div className="max-w-5xl mx-auto flex flex-col sm:flex-row items-center justify-between gap-6">
+
+    
+        <p className="text-white text-xl tracking-widest uppercase py-5">
+          © 2026 · Aziz Benothmen
+        </p>
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/weather.jsx
+++ src/pages/weather.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import CardWeather from "../components/CardWeather"
+import Footer from "../components/Footer"
 
 const Weather = () => {
   return (
-    <>
-   
-          <CardWeather/>
+    
+   <div className='-m-30'>
+     <CardWeather/>
+     <Footer/>
 
-    </>
+   </div>
+         
+
+    
     
       
     
