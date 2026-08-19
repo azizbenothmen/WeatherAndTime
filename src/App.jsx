cf49b6619f277f85ea4e1f5ee90705a4bc69d270
azizbenothmen <aziz.benothmen@enicar.ucar.tn>
2026-08-19 17:05:01 +0100

    fixing imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Weather from '/src/pages/Weather.jsx'
+import Weather from '/src/pages/weather.jsx'
 import Time from "/src/pages/Time.jsx"
 import './App.css'
 import { ToastContainer } from 'react-toastify'
